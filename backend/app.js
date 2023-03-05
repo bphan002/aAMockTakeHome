36795434672347830bd967a86bf7ca32accabf0d
Billy <billy.phan4@gmail.com>
2023-03-05 15:22:11 -0800

    remove env

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import * as dotenv from'dotenv'
+import * as dotenv from 'dotenv'
 dotenv.config()
 import mongoose from 'mongoose'
 import Coffee from './model/Coffee.js' //test 
